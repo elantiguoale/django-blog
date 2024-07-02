4d6052b87d5eed54cf2b49132355d91e2d52af7f
Miguel Hernandez <miguelalejandroh@hotmail.com>
2024-07-02 16:50:25 +0000

    added git ignore and env.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-87!^#k5qefmm&__2yb8e^$oa$38h=7@kfi@&0x+jud=^j7#vz1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-elantiguoale-djangoblog-a6xg7dtku1x.ws-eu114.gitpod.io'],'.herokuapp.com'
 
