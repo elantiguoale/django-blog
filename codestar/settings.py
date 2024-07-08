bd7f1eb395a6747fb46a1294d3a01bab21638e84
Miguel Hernandez <miguelalejandroh@hotmail.com>
2024-07-08 18:58:54 +0000

    fixed a url problem and trying to make it run, also set up modals

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-87!^#k5qefmm&__2yb8e^$oa$38h=7@kfi@&0x+jud=^j7#vz1'
+SECRET_KEY = 'os.environ.get("SECRET_KEY")'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-elantiguoale-djangoblog-a6xg7dtku1x.ws-eu114.gitpod.io'],'.herokuapp.com'
+ALLOWED_HOSTS = ['8000-elantiguoale-djangoblog-a6xg7dtku1x.ws-eu115.gitpod.io','.herokuapp.com']
 
 
 # Application definition
@@ -89,6 +89,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://8000-elantiguoale-djangoblog-a6xg7dtku1x.ws-eu115.gitpod.io",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
