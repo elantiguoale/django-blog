3f9db5a8f050c903b9649d63b371a3f30898b708
Miguel Hernandez <miguelalejandroh@hotmail.com>
2024-07-10 11:36:08 +0000

    removed an import that never existed, only in my imagination

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.auth.models import Post
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
