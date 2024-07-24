60ba5a95311343e544b6cffcd4e7d9da32c365d8
Miguel Hernandez <miguelalejandroh@hotmail.com>
2024-07-24 00:43:24 +0000

    static files message

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status = 1)
     ##template_name = "post_list.html"
     template_name = "blog/index.html"
-    paginate_by = 6
+    paginate_by = 4
 
 
 
