f4e3648454d84c697dd4b2186b92af38bc9a3b55
Miguel Hernandez <miguelalejandroh@hotmail.com>
2024-08-20 23:16:53 +0000

    added the about me app on the page and its working

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,8 @@ from .models import Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
+
+# Register your models here.
 class PostAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'slug', 'status', 'created_on')
@@ -13,6 +15,4 @@ class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
     
 
-
-# Register your models here.
 admin.site.register(Comment)
